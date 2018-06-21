ab1da1a8a7b10067ceae3e128e65d8b0c3b72f1b
Your Name <you@example.com>
2018-06-21 00:15:43 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+i=0
+#we start our counter variable i =0
+while i < array.length
+#while i is less than .length of array
+yield array[i]
+#yield the argument array
+i+=1
+#increase counter by 1
+end
+return array
 end
